aead3675c981fd0dca69821f073d2643b1202258
naomu <muramatsu2023@gmail.com>
2024-02-09 01:37:53 +0900

    japanese, chinese, italian foodのアソシエーション完了

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,7 @@
 class Food < ApplicationRecord
     has_many :order_foods#OrderFoodモデルにfood_idを複数持つ
     belongs_to :shop#Shopモデルに所属
+    has_many :japanese_foods
+    has_many :chinese_foods
+    has_many :italian_foods
 end
