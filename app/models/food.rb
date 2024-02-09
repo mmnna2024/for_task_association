782d0285ea8ee502994bd8bf728eec22abe966bb
naomu <muramatsu2023@gmail.com>
2024-02-09 14:36:55 +0900

    各フードクラスとFood,Shop,OrderFoodクラスのアソシエーション完了

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,7 +2,4 @@ class Food < ApplicationRecord
     has_many :order_foods#OrderFoodモデルにfood_idを複数持つ
     has_many :orders, through: :order_foods#order_foodモデルを介してOrderモデルと多対多の関係を持つ
     belongs_to :shop#Shopモデルに所属
-    has_many :japanese_foods
-    has_many :chinese_foods
-    has_many :italian_foods
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,7 +1,4 @@
 class Shop < ApplicationRecord
     has_many :foods#Foodモデルに複数のshop_idを持つ
     has_many :addresses,as: :addressable
-    has_many :japanese_foods
-    has_many :chinese_foods
-    has_many :italian_foods
 end
