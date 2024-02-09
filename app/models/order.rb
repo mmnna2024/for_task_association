8412da18a08570e0da1470e4eb1f29a9ab3fa37f
naomu <muramatsu2023@gmail.com>
2024-02-09 13:53:54 +0900

    orderモデルとaddressモデルのアソシエーション完了

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ApplicationRecord
     has_many :order_foods#OderFoodクラスに複数のorder_idを持つ
     has_many :foods, through: :order_foods
     belongs_to :customer
+    belongs_to :address
 end
